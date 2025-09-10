de00dbfce77a508dad64dd67f6905032e9a2890d
LilKitten <olivemasterm@gmail.comn>
2025-09-10 01:54:58 -0400

    choto

---
+++ src/runWithProperties.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+
+public class runWithProperties {
+    public static void main(String[] args) {
+        boolean withError = false;
+        try {
+            if (args != null) {
+                for (String obj : args) {
+                    if (obj.equals("gitler")) {
+                        System.out.println("priver Egor");
+                    }
+                    if (obj.equals("--help")) {
+                        System.out.println("this is HELPER with your stupid brain");
+                    }
+                }
+            }
+        } catch (Exception error) {
+            withError = true;
+        }
+        System.out.println("Nikifor negr");
+    }
+}
