77c09f69932d3215b80ac05d67306132b1b8be0c
LilKitten <olivemasterm@gmail.comn>
2025-09-10 01:21:24 -0400

    choto

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a Name Surname age ang go fuck yourself : ");
+        System.out.print("Enter Login: ");
+        String login = scanner.next();
+        System.out.print("Enter Login: ");
+        String password = scanner.next();
+
+        System.out.printf("Login : %s\nPassword : %s", login, password);
+    }
+}
