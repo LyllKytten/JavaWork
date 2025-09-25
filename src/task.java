02ed838ea67d47321d09c5706ab153037bcecd3f
LyllKytten <olivemasterm@gmail.com>
2025-09-25 11:45:23 -0700

    Lesson 9 25 2025

--- src/task.java
+++ src/task.java
@@ -2,20 +2,25 @@ import java.util.Scanner;
 
 public class task {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int workNum = num;
-        String revNum = "";
-        while (workNum > 0) {
-            revNum += workNum % 10;
-            workNum/=10;
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                matrix[i][j] = matrix[i][j] + matrix[i][matrix.length-1];
+                matrix[i][matrix.length-1] = matrix[i][j] - matrix[i][matrix.length-1];
+                matrix[i][j] = matrix[i][j] - matrix[i][matrix.length-1];
+            }
         }
-        int revNumI = Integer.parseInt(revNum);
-        System.out.println(revNumI);
-        if (num == revNumI) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not a Palindrome");
+
+        for (var mas : matrix) {
+            for (var num : mas) {
+                System.out.printf("%d ", num);
+            }
+            System.out.println();
         }
     }
 }
---
+++ src/useableFunctions/howToChangeNumsLoc.java
@@ -0,0 +1,24 @@
+package useableFunctions;
+
+public class howToChangeNumsLoc {
+    public static void main(String[] main) {
+        //                                 3 5 4      ====     5 4 3                              \\
+
+//                                 num1 = 3 + 5 + 4;  (num1+num2+num3)
+//                                 num3 = 12 - 5 - 4; (num1-num2-num3)
+//                                 num2 = 12 - 5 - 3; (num1-num2-num3)
+//                                 num1 = 12 - 4 - 3; (num1-num2-num3)
+
+//    5 6   num1 = 5, num2 =6  \\  || num1 = num1+num2; num2=num1-num2; num1=num1-num2;
+
+//int num1 = 3;
+//int num2 = 5;
+//int num3 = 4;
+//int temp = num2;
+//
+//num2 = num3;
+//num3 = num1;
+//num1 = temp; // не оч решение
+
+    }
+}
